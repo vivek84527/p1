5ab5d21557cfa4d10b24b9873e9cc33d611d41a3
vivek84527 <vivek.singh_cs18@gla.ac.in>
2019-11-13 11:58:50 +0530

    Initial commit

---
+++ GLA.py
@@ -0,0 +1,12 @@
+class GLA:
+    def btch(self):
+        print("Btec Program")
+    def bca(self):
+        print("BCA Program")
+    def bba(self):
+        print("BBA Program")
+
+con=GLA()
+con.bca()
+con.bba()
+con.btch()
